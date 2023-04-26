2948f980d8839ae2d9590ada7e792330c3dfad0f
Arielli Szeremeta Fioravante <arielli_sf_@hotmail.com>
2023-04-26 11:20:13 -0300

    Primeira requisição

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
-console.log("Sucesso")
+
+const offset = 0
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=10'
+
+// requisição
+fetch(url)
+
+    // faça alguma coisa => then
+
+    // primeiro then retorna a promise (converte como json)
+
+    // utilizando arrow function "=>" no callback
+
+    .then((response) => response.json())
+
+    .then((jsonBody) => console.log(jsonBody))
+
+    // quando da erro
+    .catch((error) => console.log(error))
+
+
